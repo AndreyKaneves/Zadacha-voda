f8400e56b4bb57c4aab8c4ce35fdbf4db699576c
AndreyKaneves <apolovinov@mail.ru>
2024-05-08 12:35:43 +0300

    Добавьте файлы проекта.

---
+++ Zadacha voda.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	double k, V, t, Hg, Hs, D, L, d, hl, Q;
+	do {
+		cout << "Введите значение k:";
+			cin >> k;
+	} while (k <= 0 || k >= 1);
+	do {
+		cout << "Введите значение V:";
+		cin >> V;
+	} while (V <= 0);
+	do {
+		cout << "Введите значение t:";
+		cin >> t;
+	} while (t >= 24);
+		cout << "Введите значение Hg:";
+		cin >> Hg;
+		cout << "Введите значение Hs:";
+		cin >> Hs;
+		cout << "Введите значение D:";
+		cin >> D;
+		cout << "Введите значение L:";
+		cin >> L;
+		cout << "Введите значение d:";
+		cin >> d;
+		cout << "Введите значение hl:";
+		cin >> hl;
+
+}
+
